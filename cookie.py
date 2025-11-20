5b36caf048f83999c78023303bd388fcd5cfe40c
Svetlana Volk <svetlenkaya78@yandex.ru>
2025-11-20 23:42:06 +0400

    Remove cookie.py and metadata files from repository

--- cookie.py
+++
@@ -1,7 +0,0 @@
-iimport requests
-
-def get_cat_image(text):
-    url = f"https://cataas.com/cat/says/{text}"
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.content
